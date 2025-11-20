919201bcd91649298e3302cd769a8e041f62f557
Alexey Franchuk <afranchuk@sanatorium-is.ru>
2025-11-21 00:59:53 +0300

    Реализовал эндпоинт - GetUserSearch для админ понели. #16. Описал команду для обработчика

---
+++ ContentFlow/ContentFlow.Application/DTOs/AdminControllerDTOs/SearchUsersQuery.cs
@@ -0,0 +1,6 @@
+﻿using ContentFlow.Application.Common;
+using MediatR;
+
+namespace ContentFlow.Application.DTOs.AdminControllerDTOs;
+
+public record SearchUsersQuery(string Query, int Limit) : IRequest<PaginatedResult<UserDto>>;
---
+++ ContentFlow/ContentFlow.Web/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using ContentFlow.Application.DTOs.AdminControllerDTOs;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContentFlow.Web.Controllers;
+
+[ApiController]
+[Route("api/admin")]
+[Authorize(Policy = "AdministrationDictionary")]
+public class AdminController : ControllerBase
+{
+    private readonly ILogger<AdminController> _logger;
+    private readonly IMediator _mediator;
+    
+    public AdminController(
+        ILogger<AdminController> logger,
+        IMediator mediator)
+    {
+        _logger = logger;
+        _mediator = mediator;
+    }
+    
+    /// <summary>
+    /// Найти пользователя по email или части имени
+    /// </summary>
+    [HttpGet("users/search")]
+    public async Task<IActionResult> GetUserSearch(
+        [FromQuery] string query,
+        [FromQuery] int limit = 10)
+    {
+        var result = await _mediator.Send(new SearchUsersQuery(query, limit));
+        
+        return Ok(result);
+    }
+}
