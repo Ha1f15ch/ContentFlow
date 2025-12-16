876728b4c8c5ef9494e5201aec97a16cf47dfb27
Alexey Franchuk <afranchuk@sanatorium-is.ru>
2025-12-16 23:55:25 +0300

    #58 Реализовал две команды для получения списка подписчиков и подписок пользователя

---
+++ ContentFlow/ContentFlow.Application/Functions/Subscriptions/Handlers/GetFollowersProfileDtoQueryHandler.cs
@@ -0,0 +1,47 @@
+﻿using ContentFlow.Application.DTOs.UserProfileSubscriptionDTOs;
+using ContentFlow.Application.Functions.Subscriptions.Queries;
+using ContentFlow.Application.Interfaces.Subscription;
+using ContentFlow.Application.Interfaces.UserProfile;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace ContentFlow.Application.Functions.Subscriptions.Handlers;
+
+public class GetFollowersProfileDtoQueryHandler : IRequestHandler<GetFollowersProfileDtoQuery, List<SubscriptionWithFollowingProfileDto>>
+{
+    private readonly ILogger<GetFollowersProfileDtoQueryHandler> _logger;
+    private readonly IUserProfileRepository _userProfileRepository;
+    private readonly ISubscriptionRepository _subscriptionRepository;
+
+    public GetFollowersProfileDtoQueryHandler(
+        ILogger<GetFollowersProfileDtoQueryHandler> logger,
+        IUserProfileRepository userProfileRepository,
+        ISubscriptionRepository subscriptionRepository)
+    {
+        _logger = logger;
+        _userProfileRepository = userProfileRepository;
+        _subscriptionRepository = subscriptionRepository;
+    }
+
+    public async Task<List<SubscriptionWithFollowingProfileDto>> Handle(GetFollowersProfileDtoQuery request,
+        CancellationToken cancellationToken)
+    {
+        _logger.LogInformation($"Get GetFollowersProfileDtoQueryHandler.Handle() called.");
+        
+        var userProfile = await _userProfileRepository.GetByUserIdAsync(request.UserId, cancellationToken);
+
+        if (userProfile == null)
+        {
+            _logger.LogError($"Get GetFollowersProfileDtoQueryHandler.Handle() returned null.");
+            throw new NullReferenceException("UserProfile not found by userId.");
+        }
+        
+        _logger.LogInformation("Fetching user profiles - on who's follower user id - {UserId}", request.UserId);
+
+        var listUserProfiles = await 
+            _subscriptionRepository.GetListSubscriptionFollowersByFollowerAsync(request.UserId, cancellationToken);
+        _logger.LogInformation("Get {CountSubscriptions} subscriptions.", listUserProfiles.Count());
+
+        return listUserProfiles;
+    }
+}
